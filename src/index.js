8d45451ded443e550b504a50b8542a7b7e2ee495
shaokiat <shaokiat@gmail.com>
2020-12-18 16:38:01 +0800

    Add NavBar from bootstrap

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from "styled-components";
 import reportWebVitals from './reportWebVitals';
+import { theme } from "./themes/theme";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
