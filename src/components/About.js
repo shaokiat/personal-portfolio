49f6c03c31b370b30f0d10ff4b7261507ddf8a14
shaokiat <shaokiat@gmail.com>
2020-12-19 21:52:48 +0800

    Add about me page

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,23 @@ import './About.css';
 
 function About() {
     return (
-
-        <div className="about" id="about">
-            <h1>About Me</h1>
-        </div>
+        <section id="about" className='about'>
+            <div className="max-width">
+                <h2 className="about-title">About Me</h2>
+                <div className="about-content">
+                    <div className="about-img">
+                        <img src="images/portrait2.jpg" alt="" />
+                    </div>
+                    <div className="about-content-right">
+                        <div className="text">I'm Shao Kiat and I'm a Programmer</div>
+                        <p>This is my portfolio website! Check me out!</p>
+                        <p>I am a Year 2 Computer Science Undergraduate at National University
+                            of Singapore and this is my attempt at building a portfolio website using React.js</p>
+                        <a href="#">Download CV</a>
+                    </div>
+                </div>
+            </div>
+        </section >
     );
 }
 
