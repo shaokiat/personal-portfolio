55106503eaf4bc0b2908ee3a729378f037412232
shaokiat <shaokiat@gmail.com>
2021-08-05 11:36:08 +0800

    Update projects

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,30 +11,30 @@ function Cards() {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem
+                            src="images/rhapp.jpg"
+                            text="Raffles Hall App"
+                            label="Hall Software Development Project"
+                            url="https://www.rhapp.lol/"
+                        />
+                        <CardItem
                             src="images/gradpad.png"
                             text="CS2103T Software Engineering Project"
                             label="Module Project"
                             url="https://ay2021s1-cs2103t-t09-1.github.io/tp/"
                         />
+                    </ul>
+                    <ul className="cards__items">
                         <CardItem
                             src="images/orbital.png"
                             text="SayonaraCorona Orbital2020 Project"
                             label="Orbital2020"
                             url="https://github.com/shaokiat/SayonaraCorona"
                         />
-                    </ul>
-                    <ul className="cards__items">
                         <CardItem
-                            src="images/img-8.jpg"
-                            text="Explore the hidden waterfall deep inside the Amazon Jungle"
-                            label="Adventure"
-                            path="/projects"
-                        />
-                        <CardItem
-                            src="images/img-6.jpg"
-                            text="Explore the hidden waterfall deep inside the Amazon Jungle"
-                            label="Adventure"
-                            path="/projects"
+                            src="images/homepage.png"
+                            text="Personal Portfolio Page"
+                            label="Winter Portfolio Project"
+                            url="https://github.com/shaokiat/personal-portfolio"
                         />
                         <CardItem
                             src="images/img-7.jpg"
