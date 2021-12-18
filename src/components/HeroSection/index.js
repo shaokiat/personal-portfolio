512506ea6506c5bcd17cf8f77f60c22f2acd00dd
shaokiat <shaokiat@gmail.com>
2021-12-18 21:22:06 +0800

    Update hero section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -5,9 +5,9 @@ import './HeroSection.css';
 const HeroSection = function () {
   return (
     <section className="hero-container">
-      <video src="/videos/video-1.mp4" autoPlay loop muted />
+      <video src="/videos/aurora.mp4" autoPlay loop muted />
       <div className="max-width">
-        <div className="content">
+        <div className="hero-content">
           <div className="text-1">Hello, my name is</div>
           <div className="text-2">Shao Kiat</div>
           <div className="text-3"><text>And I&apos;m a Computer Science Undergraduate</text></div>
