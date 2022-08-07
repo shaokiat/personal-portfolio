ebd2e0fc2510f256de2289ed58624a01b497f282
shaokiat <shaokiat@gmail.com>
2022-08-07 20:03:57 +0800

    feat: update navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,27 +5,59 @@ import './Navbar.css'
 
 const Navbar = function () {
   const [click, setClick] = useState(false)
-  const [button, setButton] = useState(true)
+  const [show, setShow] = useState(true)
+  const [lastScrollY, setLastScrollY] = useState()
+  const [showBg, setShowBg] = useState(false)
 
   const handleClick = () => setClick(!click)
   const closeMobileMenu = () => setClick(false)
 
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false)
-    } else {
-      setButton(true)
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY < 90) {
+        setShowBg(false)
+      } else {
+        setShowBg(true)
+      }
+      if (window.scrollY >= lastScrollY) {
+        // if scroll down hide the navbar
+        setShow(false)
+      } else {
+        // if scroll up show the navbar
+        setShow(true)
+      }
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY)
     }
   }
 
   useEffect(() => {
-    showButton()
-  }, [])
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar)
 
-  window.addEventListener('resize', showButton)
+      // cleanup function
+      return () => {
+        window.removeEventListener('scroll', controlNavbar)
+      }
+    }
+  }, [lastScrollY])
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY < 90 && !click) {
+        setShowBg(false)
+      } else {
+        setShowBg(true)
+      }
+    }
+  }, [click])
 
   return (
-    <nav className="navbar">
+    <nav
+      className={`navbar-active ${!show && 'navbar-hidden'} ${
+        !showBg && 'navbar-bg'
+      }`}
+    >
       <div className="navbar-container">
         <Link
           activeClass="active"
@@ -50,7 +82,7 @@ const Navbar = function () {
               to="main"
               spy
               smooth
-              offset={-70}
+              offset={0}
               duration={500}
               className="nav-links"
               onClick={closeMobileMenu}
@@ -64,7 +96,7 @@ const Navbar = function () {
               to="about"
               spy
               smooth
-              offset={-70}
+              offset={0}
               duration={500}
               className="nav-links"
               onClick={closeMobileMenu}
@@ -78,7 +110,7 @@ const Navbar = function () {
               to="skills"
               spy
               smooth
-              offset={-70}
+              offset={0}
               duration={500}
               className="nav-links"
               onClick={closeMobileMenu}
@@ -92,7 +124,7 @@ const Navbar = function () {
               to="projects"
               spy
               smooth
-              offset={-70}
+              offset={0}
               duration={500}
               className="nav-links"
               onClick={closeMobileMenu}
@@ -106,7 +138,7 @@ const Navbar = function () {
               to="contact"
               spy
               smooth
-              offset={-70}
+              offset={0}
               duration={500}
               className="nav-links"
               onClick={closeMobileMenu}
