9c1ffd9b855e54e8859df1847b4fc973c5042959
shaokiat <shaokiat@gmail.com>
2023-01-10 11:17:54 +0800

    update profile image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ const About = function () {
         <h1 className="about-title">About Me</h1>
         <div className="about-content">
           <div className="column left">
-            <img src="images/portrait.png" alt="" />
+            <img src="images/portrait.jpg" alt="" />
           </div>
           <div className="column right">
             <div className="text">
