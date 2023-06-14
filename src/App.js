746c36989b92655fbcc62aa8cac6a6c810f6869a
shaokiat <shaokiat@gmail.com>
2023-06-14 16:36:14 +0800

    add cal.com

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import Cal, { getCalApi } from '@calcom/embed-react'
 
 const App = function () {
+  useEffect(() => {
+    ;(async function () {
+      const cal = await getCalApi()
+      cal('floatingButton', { calLink: 'shaokiat/15min' })
+      cal('ui', {
+        styles: { branding: { brandColor: '#242424' } },
+        hideEventTypeDetails: false,
+      })
+    })()
+  }, [])
   return (
     <Router>
       <Navbar />
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,24 +7,31 @@ const Contact = function () {
       <div className="max-width">
         <h1 className="contact-title">Contact Me</h1>
         <div className="contact-content">
-          <div className="social-icons-contact">
-            <a
-              className="social-icon-link-github"
-              href="https://github.com/shaokiat"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="GitHub"
-            >
-              <i className="fab fa-github" />
-            </a>
-            <a
-              className="social-icon-link-linkedin"
-              href="https://www.linkedin.com/in/shaokiat/"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="LinkedIn"
-            >
-              <i className="fab fa-linkedin" />
+          <div className="">
+            <div className="social-icons-contact">
+              <a
+                className="social-icon-link-github"
+                href="https://github.com/shaokiat"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="GitHub"
+              >
+                <i className="fab fa-github" />
+              </a>
+              <a
+                className="social-icon-link-linkedin"
+                href="https://www.linkedin.com/in/shaokiat/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="LinkedIn"
+              >
+                <i className="fab fa-linkedin" />
+              </a>
+            </div>
+          </div>
+          <div className="caldotcom-link">
+            <a href="https://cal.com/shaokiat" target="_blank" rel="noreferrer">
+              cal.com/shaokiat
             </a>
           </div>
         </div>
